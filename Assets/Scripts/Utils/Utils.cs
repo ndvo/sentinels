6a3e9f661394125671cf57fe01d44dfdebe05448
Nelson do Vale Oliveira <noliveira@ginger.io>
2021-12-29 10:55:26 -0300

    feat(utils): symmetric apply
    Provides a feature where one collection is applied a transformation to the first half and a second (potentially mirrored) transformation to the second half, leaving the last one untransformed.
    
    The goal is to be able to transform the mirrored parts of ships with simple commands by ordering them in such a manner that one half is placed before the other half.

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -20,6 +20,46 @@ namespace Utils
         }
     }
 
+    public static class Iterables
+    {
+        
+        /// <summary>
+        /// Apply a given action to an iterable in a symmetrical fashion.
+        ///
+        /// By "symmetrical" we mean that the mirror transformation will applied for elements in the second half.
+        /// If the iterable has odd length, the transformation will be applied normally to the last element.
+        ///
+        /// </summary>
+        /// <param name="iterable">
+        /// Iterable is the collection of elements you wish to apply a function in a symmetrical way to.
+        /// </param>
+        /// <param name="transformation">
+        /// The action to be done to each element of the first half of the collection.
+        /// </param>
+        /// <param name="mirror">
+        /// The action to be done to each element of the second half of the collection.
+        /// </param>
+        public static void SymmetricalApply<T>(
+            IEnumerable<T> iterable,
+            Action<T> transformation,
+            Action<T> mirror
+            )
+        {
+            var enumerable = iterable as T[] ?? iterable.ToArray();
+            var n = enumerable.Count();
+            if (n == 0) return;
+            var odds = n % 2;
+            var half = odds % 2 == 0 ? n/2: (n - 1) /2;
+            for (var i = 0; i < n - odds; i++)
+            {
+                var el = enumerable[i];
+                if (i < half) transformation(el);
+                else mirror(el);
+            }
+            if (odds > 0) transformation(enumerable[n - 1]);
+        }
+    }
+
     /// <summary>
     /// Defines project extension methods as described here:
     /// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods
---
+++ Assets/Tests/TestUtils.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using Assert = UnityEngine.Assertions.Assert;
+
+public class Case
+{
+    public float X;
+    public float Y;
+    public readonly float result;
+    
+    public Case(float x, float y, float result)
+    {
+        X = x;
+        Y = y;
+        this.result = result;
+    }
+
+}
+
+public class TestUtils
+{
+        [OneTimeSetUp]
+        public void Setup()
+        {
+        }
+        
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestSymmetricalApply()
+        {
+            var cases = new[]
+            {
+                // even number of elements
+                new [] {new Case(1f, 2f, 2), new Case(2f, 1f, 1)},
+                // odd number of elements
+                new [] {new Case(1f, 2f, 2), new Case(2f, 1f, 1), new Case(1f, 2f, 2)}, 
+            };
+            foreach (var c in cases)
+            {
+                Utils.Iterables.SymmetricalApply( c, (r) => r.X += 1, (r) => r.X -= 1 );
+                Assert.AreApproximatelyEqual(2, c[0].X, "add to the first");
+                Assert.AreApproximatelyEqual(1, c[1].X, "subtract to the second");
+                if (c.Length > 2)
+                {
+                    Assert.AreApproximatelyEqual(2, c[2].X, "add to the last odd");
+                }
+            }
+        }
+
+}
