e05cb0ac9cf7ae5e47beeeeb37a84395b85bf1c3
Nelson do Vale Oliveira <noliveira@ginger.io>
2021-12-31 10:16:11 -0300

    chore(TestProtonLegacy): remove boilerplate code

--- Assets/Tests/TestProtonLegacy.cs
+++ Assets/Tests/TestProtonLegacy.cs
@@ -10,30 +10,18 @@ public class TestProtonLegacy
         "Assets/Prefabs/EnemyHeadQuarters.prefab");
     private readonly GameObject _prefab = AssetDatabase.LoadAssetAtPath<GameObject>(
         "Assets/Prefabs/ProtonLegacy1.prefab");
-    // A Test behaves as an ordinary method
-    [Test]
-    public void TestProtonLegacySimplePasses()
-    {
-        // Use the Assert class to test conditions
-    }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
     [UnityTest]
     public IEnumerator TestProtonLegacyWithEnumeratorApplyGenome()
     {
-        var headquarters = Object.Instantiate(headquartersPrefab, new Vector3(), Quaternion.identity);
+        var headquarters = Object.Instantiate(_headquartersPrefab, new Vector3(), Quaternion.identity);
         headquarters.name = "EnemyHeadQuarters";
-        var ship = Object.Instantiate(prefab, new Vector3(), Quaternion.identity);
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
+        var ship = Object.Instantiate(_prefab, new Vector3(), Quaternion.identity);
         var protonManager = ship.GetComponent<ProtonLegacy>();
         var body = protonManager.GetParts("ShipBody")[0];
         var bodyPosition = body.position.z;
         yield return null;
         var bodyPosition2 = body.position.z;
         Assert.AreNotEqual(bodyPosition, bodyPosition2, "Should have changed initial position.");
-
-
     }
 }
