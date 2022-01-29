c0623e5e2b96ac66ce58169c79f4bf2a6b91c2ad
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-01-29 18:58:04 -0300

    feat(SpaceStationBuilder): instantiage game parts from maze
    SpaceStationBuilder.cs uses a Maze Generation Algorithm to decide where to create each part of a space station.
    
    The maze is 2D array containing Directions. We use the directions basically to rotate the Game Objects and distribute the parts in a grid around the center of the SpaceStationBuilder GameObject.
    
    The size of the grid is the size of the maze times _cellSize to allow the different part to avoid collision.

--- Assets/Scripts/SpaceStationBuilder.cs
+++ Assets/Scripts/SpaceStationBuilder.cs
@@ -1,25 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
-using SentinelsUtils;
+using System;
+using System.Linq;
+using MazeGeneration;
+using Utils;
 using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class SpaceStationBuilder : MonoBehaviour
 {
-    private readonly string _spaceStationAssetFolder = "Assets/Prefabs/";
-    private readonly string _prefabExtension = ".prefab";
+    private GameObject _commonPrefab;
     private GameObject _core1Prefab;
     private GameObject _core2Prefab;
     private GameObject _panelsPrefab;
-    private GameObject _commonPrefab;
     private GameObject _regularConnectionPrefab;
     private GameObject _tubularConnectionPrefab;
     private GameObject[] _corePrefabs;
-    private GameObject[] _shortPrefabs;
     private GameObject[] _longPrefabs;
-    private readonly System.Random _random = new System.Random(SentinelsUtils.Time.UnixNow());
-    
-    void Start()
+    private GameObject[] _shortPrefabs;
+    private IMazeAlgorithm _mazeGeneraton;
+    private int _cellSize = 15;
+    private int _maxSize = 5;
+    private int _minSize = 3;
+    private readonly System.Random _random = new System.Random(Utils.Time.UnixNow());
+    private const string PrefabExtension = ".prefab";
+    private const string SpaceStationAssetFolder = "Assets/Prefabs/SpaceStation/";
+
+    public void SetStationsMaxSize(int newMaxSize, int newMinSize)
+    {
+        _maxSize = newMaxSize;
+        _minSize = newMinSize;
+    }
+
+    public void Start()
+    {
+        _setupPrefabs();
+        var sizeX = _random.Next(_minSize, _maxSize);
+        var sizeY = _random.Next(_minSize, _maxSize);
+        _mazeGeneraton = new WilsonAlgorithm(sizeX, sizeY);
+        var maze = _mazeGeneraton.CreateMaze();
+        var stationParts = (from i in 
+                new int[maze.GetLength(0) * maze.GetLength(1)]
+            select _createConnection()).ToArray();
+        var positionGrid = Utils.Iterables.CreateGrid(
+            (from n in  Utils.Iterables.BalancedRange(sizeX) select n * _cellSize).ToArray(), 
+            (from n in Utils.Iterables.BalancedRange(sizeY) select n * _cellSize).ToArray() 
+            );
+        for (int i = 0; i < stationParts.Length; i++)
+        {
+            var col = Math.Abs(i % maze.GetLength(1));
+            var row = Math.Abs(i / maze.GetLength(0));
+            _transformPosition(stationParts[i], positionGrid[row, col]);
+            _rotatePiece(stationParts[i], maze[row, col]);
+        }
+    }
+
+    private void _transformPosition(GameObject part, int[] position)
+    {
+        part.transform.position += new Vector3(position[0], 0, position[1]);
+    }
+
+    private void _setupPrefabs()
     {
         _core1Prefab = AssetDatabase.LoadAssetAtPath<GameObject>(
             SpaceStationAssetFolder + "Core1" + PrefabExtension
