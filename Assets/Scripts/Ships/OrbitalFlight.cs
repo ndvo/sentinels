c9e9e1b05bfe99db74164760fead4d3920616f6c
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-02-05 18:53:26 -0300

    fix(Orbital Flight): make the ship look at its future position
    After noticing a bug that resulted in the ship eventually flying
    sideways, several approaches were attempted to fix it.
    
    The bug came about because the ship would face a "Direction" (North,
    South, West or East), meaning far away points that map to intuitive
    directions. The problem is that in a circular orbit this would mean that
    when flying further from the certer of the Earth the ship would twist.
    This effect compounded resulting in weird flying. Also, when the ship
    reverted its direction it would not revert the inclination.
    
    The first attempt to fix this was to manually apply a rotation fix by
    manipulating the Quartenion. This approach was unsuccessful.
    
    A second approach was to replace the provided LooAt function with a
    custom one that would handle this issue. This was also not successful.
    
    A third approach, using Quartenion operations, consisted in creating a
    Quartenion that was the inverse of the previous direction, apply it to
    remove the direction from the Object rotation and then apply a new
    quartenion built with the current direction. The idea was that the
    object would be rotated by the Direction and then by the RotateAround function.
    In the next frame we would like to preserve the effects of the
    RotateAround function, but remove the effects of the Direction. So, an
    Inverse of the Direction Quartenion was created and applied and then a
    new Quartenion would be applied with only the new Direction.
    
    This approach was successful in fixing the knwon issues, but the orbital
    flight would still eventually result in a weird rotation due to the fact
    that the game simulates a 2D top view flight over a sphere. As a result
    a flight on the edges is faster than a flight in the middle, with
    consequences to the ship rotation.
    
    Finally, a new solution was to create a new GameObject (_aim) that would
    be constantly poisitoned ahead of the ship using the same function
    RotateAround. Regardless of the issues of applying a 2D plane over a
    sphere, the ship would always face the direction it is going to.

--- Assets/Scripts/Ships/OrbitalFlight.cs
+++ Assets/Scripts/Ships/OrbitalFlight.cs
@@ -1,62 +1,71 @@
+using System;
 using UnityEngine;
+using Utils;
+using Time = UnityEngine.Time;
 using Vector3 = UnityEngine.Vector3;
 
 public class OrbitalFlight : MonoBehaviour
 {
-    
+
     public float speed;
-    private int _verticalMovement = 0;
-    private int _horizontalMovement = 0;
+    private Position _direction = new Position(0, 0);
+    private Position _previousDirection = new Position(0, 0);
     private float _deltaTimeSpeed = 0;
+    private GameObject _aim;
     [SerializeField] private bool inertia;
+
+    public void Awake()
+    {
+        _aim = new GameObject();
+    }
 
     private void FixedUpdate()
     {
         _deltaTimeSpeed = speed * Time.deltaTime;
-        SetHorizontalMovement();
-        SetVerticalMovement();
-        transform.RotateAround(
-            new Vector3(0f, 0f, 0f),
-            Vector3.left,
-            _deltaTimeSpeed *  _verticalMovement
-            );
-        transform.RotateAround(
-            new Vector3(0f, 0f, 0f),
-            Vector3.forward,
-            _deltaTimeSpeed * _horizontalMovement
-            );
+        _setNewDirection();
+        _move(transform, _deltaTimeSpeed);
+        _positionAim();
         LookAtDirection();
     }
 
-    private void SetHorizontalMovement()
+    private void _move(Transform t, float moveSpeed)
     {
-        if (Input.GetKey("left"))
-            _horizontalMovement = 1;
-        if (Input.GetKey("right"))
-            _horizontalMovement = -1;
-        if (Input.GetKey("up") || Input.GetKey("down"))
-            _horizontalMovement = 0;
+        t.RotateAround(
+            new Vector3(0f, 0f, 0f),
+            Vector3.left,
+            moveSpeed * _direction.y
+        );
+        t.RotateAround(
+            new Vector3(0f, 0f, 0f),
+            Vector3.forward,
+            moveSpeed * _direction.x
+        );
     }
 
-    private void SetVerticalMovement()
+    private void _setNewDirection()
     {
-        if (Input.GetKey("up"))
-            _verticalMovement = -1;
+        _previousDirection = new Position(_direction.x, _direction.y);
+        if (Input.GetKey("left"))
+            _direction = Direction.East;
+        if (Input.GetKey("right"))
+            _direction = Direction.West;
         if (Input.GetKey("down"))
-            _verticalMovement = 1;
-        if (Input.GetKey("right") || Input.GetKey("left"))
-            _verticalMovement = 0;
+            _direction = Direction.South;
+        if (Input.GetKey("up"))
+            _direction = Direction.North;
+    }
+
+    private void _positionAim()
+    {
+        _aim.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        _aim.transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z,
+            transform.rotation.w);
+        _move(_aim.transform, speed);
     }
 
     private void LookAtDirection()
     {
-        if (Input.GetKey("up"))
-            transform.LookAt(transform.position + new Vector3(0f, 0f, 1000f));
-        if (Input.GetKey("down"))
-            transform.LookAt(transform.position + new Vector3(0f, 0f, -1000f));
-        if (Input.GetKey("right"))
-            transform.LookAt(transform.position + new Vector3(1000f, 0f, 0f));
-        if (Input.GetKey("left"))
-            transform.LookAt(transform.position + new Vector3(-1000f, 0f, 0f));
+        transform.LookAt(_aim.transform.position);
     }
+
 }
