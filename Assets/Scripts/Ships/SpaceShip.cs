949f1a86a9821346a8252696542ec35acd467ea0
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-02-06 22:13:48 -0300

    feat(SpaceShip): add Shields
    Adds shields to the SpaceShip.
    
    Both the Sentinel and Enemy Ships will have shields.

--- Assets/Scripts/Ships/SpaceShip.cs
+++ Assets/Scripts/Ships/SpaceShip.cs
@@ -7,19 +7,23 @@ using Object = UnityEngine.Object;
 
 public class SpaceShip : MonoBehaviour
 {
+    public float energyLevel = 1000;
     private ParticleSystem _explosionPS;
     private AudioSource _explosionAudio;
+    private GameObject _shield;
     
     private void Start()
     {
         _explosionPS = GameObject.Find("Explosion").GetComponent<ParticleSystem>();
         _explosionAudio = GetComponent<AudioSource>();
+        _shield = GameObject.Find("Shield");
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("SpaceStation"))
         {
+            TakeDamage(1);
             _explode();
         }
     }
@@ -27,7 +31,17 @@ public class SpaceShip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void FixedUpdate()
+    {
+        _shield.SetActive(energyLevel >= 900);
+    }
+
+    public float TakeDamage(float damage)
+    {
+        energyLevel -= damage;
+        return damage;
     }
 
     private void _explode() {
