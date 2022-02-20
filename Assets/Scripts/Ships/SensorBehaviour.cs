0b3fb8efbd58c14fe2f9317786d9047ea85e1d1b
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-02-20 20:33:05 -0300

    feat(Sensor): move sensor features to SensorBehaviour.cs

--- Assets/Scripts/Ships/SensorBehaviour.cs
+++ Assets/Scripts/Ships/SensorBehaviour.cs
@@ -1,29 +1,48 @@
+using System.Linq;
+using Ships;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class SensorBehaviour : MonoBehaviour
 {
     private TreeNode treeNode;
+    public GameObject sensorTemplate;
+    public int level;
+    public bool root = false;
+    private SensorBehaviour parentSensor;
 
-    private bool blocked;
-    private bool leaf;
-
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        blocked = false;
+        parentSensor = transform.parent.gameObject.GetComponent<SensorBehaviour>();
+        ResetLevel();
+        ResetTreeNode();
+        ResetChildObjects();
+        CreateChildren();
     }
 
-    public void SetLeaf()
+    private void ResetTreeNode()
     {
-        this.treeNode.Leaf = true;
+        treeNode.Blocked = false;
+        treeNode.Leaf = level == 0;
+        treeNode.Position = transform.position;
     }
 
-    public void SetTreeNode(TreeNode treeNode)
+    private void ResetLevel()
     {
-        this.treeNode = treeNode;
+        level = (parentSensor != null) ? parentSensor.level - 1 : level;
     }
 
-    public TreeNode GetTreeNode(TreeNode treeNode)
+    private void ResetChildObjects()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.gameObject.GetComponent<SensorBehaviour>() != null)
+                Destroy(child.gameObject);
+        }
+    }
+
+
+    public TreeNode GetTreeNode()
     {
         return this.treeNode;
     }
@@ -43,9 +62,33 @@ public class SensorBehaviour : MonoBehaviour
         treeNode.Blocked = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateChildren()
     {
-        
+        if (level <= 0) return;
+        var forward = Object.Instantiate(gameObject, transform);
+        var left = Object.Instantiate(gameObject, transform);
+        var right = Object.Instantiate(gameObject, transform);
+        var newNodes = new[] {forward, left, right};
+        // Position child sensors
+        forward.GetComponent<SensorBehaviour>().Move(0, 2);
+        left.GetComponent<SensorBehaviour>().Move(-2, 0);
+        right.GetComponent<SensorBehaviour>().Move(2, 0);
+        treeNode.Children = (from n in newNodes 
+            select n.GetComponent<SensorBehaviour>().treeNode).ToArray();
+    }
+
+    public void Move(int directionY, int directionZ)
+    {
+        //transform.LookAt(transform.position + new Vector3(directionY, 0, directionZ));
+        if (directionZ != 0) transform.RotateAround(
+            Vector3.zero, 
+            Vector3.right,
+            directionZ
+        );
+        if (directionY != 0) transform.RotateAround(
+            Vector3.zero, 
+            Vector3.forward,
+            directionY
+        );
     }
 }
