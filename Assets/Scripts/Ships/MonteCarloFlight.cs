59a80edfa1136ede58f45b49d1af2ecefe8713a2
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-02-28 19:25:11 -0300

    fix(MonteCarloFlight): avoid null errors

--- Assets/Scripts/Ships/MonteCarloFlight.cs
+++ Assets/Scripts/Ships/MonteCarloFlight.cs
@@ -25,20 +25,22 @@ namespace Ships
         protected override void _setNewDirection()
         {
             directionTime += Time.deltaTime;
-            if (directionTime >= 0.2f) {
+            if (directionTime >= 0.1f) {
                 directionTime = 0.0f;
                 var path = mtSearch.ComputePath(sensor.GetTreeNode(), target.transform.position);
-                if (path.Nodes != null && path.Nodes.Count > 0)
-                {
-                    sensor.ShowIfInList(path.Nodes);
-                    var next = path.Nodes[1];
-                    var position = transform.position;
-                    var vertical = next.Position.z - position.z;
-                    var horizontal = next.Position.x - position.x;
-                    Direction = Math.Abs(vertical) > Math.Abs(horizontal) 
-                        ? new Position(0, vertical > 0 ? 1 : -1) 
-                        : new Position(horizontal > 0 ? 1 : -1, 0);
-                }
+                if (path.Nodes == null || path.Nodes.Count <= 1) return;
+                sensor.ShowIfInList(path.Nodes);
+                var nodes = path.Nodes;
+                var next = nodes[1];
+                var position = transform.position;
+                var nextPosition = next.Position;
+                var vertical = nextPosition.z - position.z;
+                var horizontal = nextPosition.x - position.x;
+                var absV = Math.Abs(vertical);
+                var absH = Math.Abs(horizontal);
+                Direction = absV > absH
+                    ? new Position(0, (int) (vertical/absV))
+                    : new Position((int) (horizontal/absH), 0);
             }
         }
 
