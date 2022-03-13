7622465911f5584a382789f2648f8fbcaf6d3487
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-03-13 16:51:14 -0300

    feat(SimpleFlight): responds to sentinel sensor

--- Assets/Scripts/Ships/SimpleFlight.cs
+++ Assets/Scripts/Ships/SimpleFlight.cs
@@ -1,11 +1,15 @@
-using UnityEngine;
+using System;
+using UnityEngine.PlayerLoop;
 using Utils;
+using Random = UnityEngine.Random;
 
 namespace Ships
 {
     public class SimpleFlight : OrbitalFlight
     {
         private SimpleSensor _sensor;
+        private SimpleSensor _sentinelDetector;
+        private EnemyBehaviour _behaviour;
 
         public override void Start()
         {
@@ -15,16 +19,63 @@ namespace Ships
             {
                 _sensor = sensorObj.GetComponent<SimpleSensor>();
             }
+            var sentinelDetectorObj = transform.Find("NavigationSensor");
+            if (sentinelDetectorObj != null)
+            {
+                _sentinelDetector = sentinelDetectorObj.GetComponent<SimpleSensor>();
+            }
+            _behaviour = GetComponent<EnemyBehaviour>();
         }
 
         protected override void _setNewDirection()
         {
-            if (_sensor.Blocked)
+            if (_behaviour is null)
+                _setDirectionRepositioning();
+            else switch (_behaviour.StateMachine.GetState())
+            {
+                case EnemyShipStates.Repositioning:
+                case EnemyShipStates.Idle:
+                    _setDirectionRepositioning();
+                    break;
+                case EnemyShipStates.Dying:
+                    return;
+                case EnemyShipStates.Fleeing:
+                case EnemyShipStates.Resisting:
+                    _setDirectionFleeing();
+                    break;
+                case EnemyShipStates.AttackingEarth:
+                    CurrentDirection = new Position(0, 0);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        
+        private void _setDirectionRepositioning()
+        {
+            if (_sensor.blocked)
             {
                 CurrentDirection = CurrentDirection.x != 0 
                     ? new Position(0, Random.value > 0.5f ? 1 : -1) 
                     : new Position(Random.value > 0.5f ? 1 : -1, 0);
             }
         }
+
+        private void _setDirectionFleeing()
+        {
+            if (_sensor.blocked)
+            {
+                _setDirectionRepositioning();
+                return;
+            }
+            var avoid = _sentinelDetector.blocking.transform.position;
+            var opposite = avoid - transform.position;
+            var oppositeX = opposite.x;
+            var oppositeZ = opposite.z;
+            CurrentDirection = oppositeX > oppositeZ
+                ? new Position(oppositeX > 0 ? 1 : 0, 0) 
+                : new Position(0, oppositeZ > 0 ? 1: 0);
+        }
+        
     }
 }
