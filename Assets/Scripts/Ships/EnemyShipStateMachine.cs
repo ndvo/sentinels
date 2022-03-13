c3d57896d6c54d37817162fab8c799f0151ea808
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-03-13 16:11:55 -0300

    feat(EnemyStateMachine): add a state machine to control enemies
    The state machine ensures that the expected behaviour won't changed by
    logic spread across multiple places.

---
+++ Assets/Scripts/Ships/EnemyShipStateMachine.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace Ships
+{
+    public enum EnemyShipStates
+    {
+        Idle,
+        Repositioning,
+        AttackingEarth,
+        Fleeing,
+        Resisting,
+        Dying
+    }
+
+    public class EnemyShipStateMachine
+    {
+
+        private EnemyShipStates _state = EnemyShipStates.Idle;
+
+        public EnemyShipStates GetState()
+        {
+            return _state;
+        }
+        
+        private void _throwInvalidState()
+        {
+            throw new Exception($"Invalid state {_state}");
+        }
+
+        public void Reposition()
+        {
+            if (_state == EnemyShipStates.Dying) _throwInvalidState();
+            _state = EnemyShipStates.Repositioning;
+        }
+
+        public void AttackEarth()
+        {
+            if (_state != EnemyShipStates.Repositioning) _throwInvalidState();
+            _state = EnemyShipStates.AttackingEarth;
+        }
+
+        public void Flee()
+        {
+            if (_state == EnemyShipStates.Dying) _throwInvalidState();
+            _state = EnemyShipStates.Fleeing;
+        }
+
+        public void Resist()
+        {
+            if (_state != EnemyShipStates.Fleeing) _throwInvalidState();
+            _state = EnemyShipStates.Resisting;
+        }
+
+    }
+}
