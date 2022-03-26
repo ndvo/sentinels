31210b186b981b34fa00a8fe30e3b35f2a1681fe
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-03-26 11:09:56 -0300

    feat(HeadquartersFlight): create a flight behaviour for enemy headquarters
    This is the first step to integrate the GeneticAlgorithm into the game.
    
    Making the enemy headquarters fly around the globe, being careful not to enter the inhabited area. From there it will send the enemy ships.

---
+++ Assets/Scripts/Ships/HeadquartersFlight.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Ships;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Utils;
+using Time = UnityEngine.Time;
+
+public class HeadquartersFlight : ShipFlight
+{
+    protected override void _orbitalFlight()
+    {
+        DeltaTimeSpeed = speed * Time.fixedDeltaTime;
+        if (Random.value < 0.0001f || !OffBoard) _setDirection();
+        _positionAim();
+        LookAtDirection();
+        _move(transform, DeltaTimeSpeed);
+    }
+    
+    private void _setDirection()
+    {
+        CurrentDirection = CurrentDirection.x != 0 
+            ? new Position(0, Random.value > 0.5f ? 1 : -1) 
+            : new Position(Random.value > 0.5f ? 1 : -1, 0);
+    }
+
+}
