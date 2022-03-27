4b04d58e08f7c3a36f7786d724eaa83bf4834e56
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-03-27 19:18:37 -0300

    feat(SentinelFlight): speed boost
    The player is now able to increase the ship speed. Holding Fire2
    increases the speed and releasing it brings the speed back to its
    natural value.

--- Assets/Scripts/Ships/SentinelFlight.cs
+++ Assets/Scripts/Ships/SentinelFlight.cs
@@ -5,8 +5,16 @@ using Time = UnityEngine.Time;
 
 namespace Ships
 {
+    
     public class SentinelFlight : ShipFlight
     {
+        private float standardSpeed;
+
+        public override void Start()
+        {
+            standardSpeed = speed;
+            base.Start();
+        }
 
         protected override void _setNewDirection()
         {
@@ -16,6 +24,9 @@ namespace Ships
             horizontal = horizontal > 0 ? 1 : horizontal < 0 ? -1 : 0;
             vertical = vertical > 0 ? -1 : vertical < 0 ? 1 : 0;
             if (horizontal != 0|| vertical != 0) CurrentDirection = new Position((int) horizontal, (int) vertical);
+            speed = (Input.GetAxisRaw("Fire2") > 0)
+                ? Mathf.Clamp(speed + 1f, standardSpeed, 10)
+                : Mathf.Clamp(speed - 1f, standardSpeed, 10); 
         }
         
     }
