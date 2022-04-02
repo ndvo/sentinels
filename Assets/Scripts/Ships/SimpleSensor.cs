424a838ad816a35926d0c4a4d403622203059379
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-04-02 20:20:20 -0300

    doc(SimpleSensor): improve documentation

--- Assets/Scripts/Ships/SimpleSensor.cs
+++ Assets/Scripts/Ships/SimpleSensor.cs
@@ -1,9 +1,12 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
+/// <summary>
+/// Implements the behaviour of a simple sensor.
+///
+/// A sensor detects collisions with objects in a specific layer.
+/// If the sensor detected something, it is blocked.
+/// It stores the blocking game object in the blocking variable.
+/// </summary>
 public class SimpleSensor : MonoBehaviour
 {
 
@@ -17,6 +20,10 @@ public class SimpleSensor : MonoBehaviour
         transform.localScale = sensorScale;
     }
 
+    /// <summary>
+    /// Detects collisions when a target object enters the sensor
+    /// </summary>
+    /// <param name="other">the colliding object</param>
     public void OnTriggerEnter(Collider other)
     {
         if (((1 << other.gameObject.layer) & layer) == 0) return;
@@ -24,6 +31,10 @@ public class SimpleSensor : MonoBehaviour
         blocking = other.gameObject;
     }
 
+    /// <summary>
+    /// Ensures the sensor status will be blocked while there is any object colliding.
+    /// </summary>
+    /// <param name="other">the colliding object</param>
     public void OnTriggerStay(Collider other)
     {
         if (((1 << other.gameObject.layer) & layer) == 0) return;
@@ -31,6 +42,12 @@ public class SimpleSensor : MonoBehaviour
         blocking = other.gameObject;
     }
 
+    /// <summary>
+    /// When an object leaves the sensor, make it non blocked.
+    ///
+    /// If there are other objects in the sensor, OnTriggerStay will make sure the sensor revert this change.
+    /// </summary>
+    /// <param name="other"></param>
     public void OnTriggerExit(Collider other)
     {
         if (((1 << other.gameObject.layer) & layer) == 0) return;
