fc36c96ebdb0c83bd46da1adf2e47ea76d4291c9
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-04-02 18:21:38 -0300

    doc(ShipDetector): improve documentation

--- Assets/Scripts/Ships/ShipDetector.cs
+++ Assets/Scripts/Ships/ShipDetector.cs
@@ -22,23 +22,42 @@ namespace Ships
             transform.localScale = new Vector3(size, size, size);
         }
 
+        /// <summary>
+        /// Detect vessels when they enter the sensor.
+        /// </summary>
+        /// <param name="other"></param>
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.layer != _shipLayer) return;
             detected.Add(other.gameObject);
         }
 
+        /// <summary>
+        /// Remove vessels from detected list when they leave sensor
+        /// </summary>
+        /// <param name="other"></param>
         private void OnTriggerExit(Collider other)
         {
             if (other.gameObject.layer != _shipLayer) return;
             detected.Remove(other.gameObject);
         }
 
+        /// <summary>
+        /// Clean up detected list.
+        ///
+        /// GameObjects in this list may have been destroyed and referencing destroyed objects can cause unexpected
+        /// behaviour.
+        /// </summary>
         private void Update()
         {
             detected = detected.Where(i => !(i is null) && i.activeSelf).ToList();
         }
 
+        /// <summary>
+        /// Returns the detected object that is closest to the provided target.
+        /// </summary>
+        /// <param name="targetPosition">a position one wants the closest object from.</param>
+        /// <returns>the closest game object</returns>
         public GameObject Closest(Vector3 targetPosition)
         {
             detected = detected
