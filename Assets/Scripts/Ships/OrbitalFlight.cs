e5d019138aaf28f41fd510edd8f7f8515d6f71d7
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-04-02 17:41:44 -0300

    doc(OrbitalFlight): improve documentation

--- Assets/Scripts/Ships/OrbitalFlight.cs
+++ Assets/Scripts/Ships/OrbitalFlight.cs
@@ -6,6 +6,17 @@ using Vector3 = UnityEngine.Vector3;
 
 namespace Ships
 {
+    /// <summary>
+    /// Orbital Flight class is responsible for controlling the flight in orbit around Earth
+    ///
+    /// It does so by leveraging Unity's RotateAround method. This choice allowed for rapid development but it has some
+    /// unfortunate consequences:
+    /// - speed is measured in angles
+    /// - the actual speed depends both on the angle speed and the distance from Earth. Luckily there is only one object
+    /// (the Moon) travelling at a different distance from Earth.
+    /// - It can be tricky to position objects manually.
+    ///
+    /// </summary>
     public class OrbitalFlight : MonoBehaviour
     {
 
@@ -20,6 +31,10 @@ namespace Ships
             CurrentDirection = Utils.Direction.North;
         }
 
+        /// <summary>
+        /// Allow a transform t to follow current movement and rotation
+        /// </summary>
+        /// <param name="t">Transform that will follow this.transform</param>
         public void MoveWithMe(Transform t)
         {
             _move(t, DeltaTimeSpeed);
@@ -27,21 +42,32 @@ namespace Ships
 
         public virtual void Update()
         {
+            // Gather of input happen here
             _setNewDirection();
         }
 
         public void FixedUpdate()
         {
+            // Actual movement happen here. This game does not use physic engine, but there is no need for movement to
+            // be computed faster.
             _orbitalFlight();
         }
 
 
+        /// <summary>
+        /// Default orbital flight sets the speed and rotate around Vector3.zero by DeltaTimeSpeed angles.
+        /// </summary>
         protected virtual void _orbitalFlight()
         {
             DeltaTimeSpeed = speed * Time.fixedDeltaTime;
             _move(transform, DeltaTimeSpeed);
         }
 
+        /// <summary>
+        /// Rotate transform t around position zero at an angle of moveSpeed in the CurrentDirection
+        /// </summary>
+        /// <param name="t">The transform to be rotated</param>
+        /// <param name="moveSpeed">The angle that determines how much to rotate.</param>
         protected void _move(Transform t, float moveSpeed)
         {
             t.RotateAround(
@@ -56,6 +82,11 @@ namespace Ships
             );
         }
 
+        /// <summary>
+        /// Placeholder function to set new direction.
+        ///
+        /// This function should collect user input for the player ship or compute the new direction for NPC.
+        /// </summary>
         protected virtual void _setNewDirection()
         {
             CurrentDirection = Utils.Direction.North;
