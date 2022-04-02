fe29f7bb15d170cb9635c4982a09d2ebc2ffbb91
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-04-02 17:35:04 -0300

    doc(HeadquartersFlight): improve documentation

--- Assets/Scripts/Ships/HeadquartersFlight.cs
+++ Assets/Scripts/Ships/HeadquartersFlight.cs
@@ -6,17 +6,28 @@ using UnityEngine.Serialization;
 using Utils;
 using Time = UnityEngine.Time;
 
+/// <summary>
+/// Controls the flight of the Headquarters
+///
+/// The purpose of the Headquarters flight is twofold:
+/// - distribute spawned ships in a less predictable way
+/// - make it harder for players to destroy the headquarter (this feature is not implemented yet and will not be
+/// implemented before the final project deadline)
+/// </summary>
 public class HeadquartersFlight : ShipFlight
 {
     public override void Update()
     {
     }
 
+    /// <summary>
+    /// Override the orbital flight to change directions in a random fashion and to fly only off board.
+    /// </summary>
     protected override void _orbitalFlight()
     {
         OffBoard = HasSpaceStationBuilder && SpaceStationBuilder.IsOffBoard(transform.position);
         DeltaTimeSpeed = speed * Time.fixedDeltaTime;
-        if (Random.value < 0.001f || !OffBoard) _setNewDirection();
+        if (Random.value < 0.0005f || !OffBoard) _setNewDirection();
         _positionAim();
         LookAtDirection();
         _move(transform, DeltaTimeSpeed);
